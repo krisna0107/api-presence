cb10f1f1b17ca6f8ed567ee2ab8c70fe5a89c595
krisna0107 <krisnayanajavista@gmail.com>
2021-04-07 16:39:48 +0700

    qr

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use SimpleSoftwareIO\QrCode\Generator;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $qrcode = new Generator;
+    $qr = $qrcode->size(500)->generate('Make a qrcode without Laravel!');
+
+    return view('welcome', [
+        'qr' => $qr
+    ]);
 });
