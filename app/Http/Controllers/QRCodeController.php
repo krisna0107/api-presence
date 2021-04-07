e32adf5264eab3e5b4520f816242eeaf7492dc4d
krisna0107 <krisnayanajavista@gmail.com>
2021-04-07 17:15:00 +0700

    qr

--- app/Http/Controllers/QRCodeController.php
+++ app/Http/Controllers/QRCodeController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Karyawan;
+use App\Models\QRCode;
 
 class QRCodeController extends Controller
 {
     //
     public static function GenerateQR()
     {
-        return Karyawan::first();
+        return QRCode::first();
     }
 }
