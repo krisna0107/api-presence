9011a9c2780c2acac1cc3150e18b17ded9eb1f41
krisna0107 <krisnayanajavista@gmail.com>
2021-08-02 17:41:19 +0700

    dev

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Device extends Model
 {
     use HasFactory;
-
+    protected $hidden = array('id', 'karyawan_id', 'created_at', 'updated_at');
     public function karyawan()
     {
         return $this->hasOne(Karyawan::class, 'id', 'karyawan_id');
